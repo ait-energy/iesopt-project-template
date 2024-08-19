13ee828f90e46053d36fab95b6c2fe3b145a6313
Stefan Strömer <stefan.stroemer@ait.ac.at>
2024-08-19 13:43:06 +0200

    finalize sample model

--- {{ cookiecutter.project_slug }}/main.py
+++ {{ cookiecutter.project_slug }}/main.py
@@ -1,7 +1,11 @@
 import iesopt
 
 
+# Parse, build, and solve the model.
 model = iesopt.run("opt/config.iesopt.yaml")
 
+# Get the results as a pandas DataFrame.
 df = model.results.to_pandas()
 
+# Print the first 5 rows of a sub-part of the DataFrame.
+print(df[df["field"] == "value"].head(5))
